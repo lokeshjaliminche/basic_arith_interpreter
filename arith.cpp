2862ba642f936fbce385c75c90ee17a67215529c
Lokesh Jaliminche <lokesh.jaliminche@gmail.com>
2020-04-08 18:43:17 -0700

    Added code to skip whitespaces

--- arith.cpp
+++ arith.cpp
@@ -1,54 +1,70 @@
 #include <iostream>
+#include <iterator>
+#include <map>
 
 using namespace std;
 
-#string myString = new string[8];
-char const *myStrings[] = {
-    "Some",
-    "Strings"
-};
-
-enum Token_type{
-     NAME,
-     NUMBER,
-     END,
-     PLUS='+',
-     MINUS='-',
-     MUL='*',
-     DIV='/',
-     PRINT=';',
-     ASSIGN='=',
-     LP='(',
-     RP=')'
+enum Token_type
+{
+	NAME,
+	NUMBER,
+	END,
+	PLUS='+',
+	MINUS='-',
+	MUL='*',
+	DIV='/',
+	PRINT=';',
+	ASSIGN='=',
+	LP='(',
+	RP=')'
 };
 
 
-Class Token
+class Token
 {
-	Token_type type;
-	double numbervalue;
-	string stringvalue
-	Token get_token();
-}
 
-Class Lexer
+	public:
+		Token_type type;
+		Token_type currToken;
+		double numbervalue;
+		string stringvalue;
+};
+
+class Lexer 
 {
-	Token getToken();
-}
+	public:
+	void get_token();
+};
 
-Class Parser
+class Parser
 {
 	double prim(bool);
 	double term(bool);
 	double expr(bool);
 
 	map<string, double> table;
-}
+};
 
 int main()
 {
 	string text;
 	cin >> text;
 	cout << "text is : " << text;	
+
+}
+
+void Lexer::get_token()
+{
+	char ch = 0;
+	Token token;
+	Token_type type;
+	do
+	{
+		if(!cin.get(ch))
+		{
+			token.currToken = END;
+			return;
+		}
+	}while(ch != '\n' && isspace(ch));
 
 }
